e0ee0c27530bd9b3fb59f08166e61917b5cf2f72
Przemek Jurkiewicz <hankusekbb@gmail.com>
2018-01-15 18:43:30 +0100

    scripts.js edit

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,9 @@
-var list = document.getElementById('list');
-var add = document.getElementById('addElem');
-add.addEventListener('click', function(e){
-	var element = document.createElement('li');
-	element.innerHTML = 'item';
-	var item = getElementByTagName('li');
-	var itemLength = item.length;
-	for (var i=0 ; i <= item.lenght ; i++) {
-		list.appendChild(element);
-	}
+var list = document.getElementsById('list');
+var add = document.getElementsById('addElem');
+add.addEventListener('click', function(e) {
+    var element = document.createElement('li');
+    element.innerHTML = 'item';
+    var item = getElementsByTagName('li');
+    var itemLength = item.length;
+    list.appendChild(element);
 });
