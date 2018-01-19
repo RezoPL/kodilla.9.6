39099cef330630a7ffea409520b1c3ca325da52b
Przemek Jurkiewicz <hankusekbb@gmail.com>
2018-01-19 20:04:16 +0100

    make some changes in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@ var list = document.getElementById('list');
 var add = document.getElementById('addElem');
 add.addEventListener('click', function(e) {
     var element = document.createElement('li');
-    element.innerHTML = 'item';
     var item = document.getElementsByTagName('li');
     var itemLength = item.length;
+    element.innerHTML = 'item' + itemLength;
     list.appendChild(element);
 });
